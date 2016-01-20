b87dd0808ec9674810dcd8bc362313ee1c0b629b
Yusuke Hirao <yusukehirao@me.com>
2016-01-20 17:11:33 +0900

    ポリフィル用モジュールを"babel-polyfil"ではなく直接"core-js"をするように変更

--- src/baserJS.ts
+++ src/baserJS.ts
@@ -1,4 +1,10 @@
-import 'babel-polyfill';
+/**
+ * babel-polyfillは使わずにcore-jsから必要な機能だけ読み取る
+ * 読み込むモジュールをできるだけ少なくしてファイルサイズを極力抑える
+ */
+import 'core-js/es6/symbol';
+import 'core-js/fn/set';
+import 'core-js/fn/map';
 
 import baser = require('./baser');
 window['baser'] = baser;
