4c34e5eef4cac03208c4e112fef083e3b2d13595
Yusuke Hirao <yusukehirao@me.com>
2017-09-26 10:38:27 +0900

    🔥 Remove unused files

--- out/baserJS.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="../typings/bundle.d.ts" />
-"use strict";
-var baser = require('./baser');
-window['baser'] = baser;
-var JQueryAdapter = require('./Class/JQueryAdapter');
-$.extend($, JQueryAdapter);
-$.extend($.fn, JQueryAdapter.prototype);
